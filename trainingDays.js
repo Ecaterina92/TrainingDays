ba709f0b75a7303151888def234f8afb0b5e3283
Ecaterina Pihurov <ecaterinaoprea92@gmail.com>
2021-04-01 16:29:37 +0300

    Second commit

--- trainingDays.js
+++ trainingDays.js
@@ -1,6 +1,6 @@
 // The scope of `random` is too loose 
 
- 
+
 const getRandEvent = () => {
   const random = Math.floor(Math.random() * 3);
   if (random === 0) {
@@ -11,39 +11,39 @@ const getRandEvent = () => {
     return 'Pentathlon';
   }
 };
- 
+
 // The scope of `days` is too tight 
 const getTrainingDays = event => {
- let days;
+  let days;
   if (event === 'Marathon') {
-     days = 50;
+    days = 50;
   } else if (event === 'Triathlon') {
-     days = 100;
+    days = 100;
   } else if (event === 'Pentathlon') {
     days = 200;
   }
- 
+
   return days;
 };
- const name = 'Nala';
+const name = 'Nala';
 // The scope of `name` is too tight 
 const logEvent = (name, event) => {
-  
+
   console.log(`${name}'s event is: ${event}`);
 };
- 
+
 const logTime = (name, days) => {
 
   console.log(`${name}'s time to train is: ${days} days`);
 };
- 
+
 const event = getRandEvent();
 const days = getTrainingDays(event);
 // Define a `name` variable. Use it as an argument after updating logEvent and logTime 
- 
- 
-logEvent(event);
-logTime(days);
+
+
+logEvent(name, event);
+logTime(name, days);
 
 const event2 = getRandEvent();
 const days2 = getTrainingDays(event2);
